38b7932bd2074a7eca67de22e107ea66197a0868
Valeriy Muserkiy <softhome65@gmail.com>
2026-03-28 06:31:31 +0300

    fix

--- task3V2.php
+++ task3V2.php
@@ -5,13 +5,13 @@ $x = (float)readline('Введите координату x: ');
 $y = (float)readline('Введите координату y: ');
 
 switch (true) {
-    case $x == 0 && $y == 0:
+    case $x === 0.0 && $y === 0.0:
         echo "Точка ($x, $y) — начало координат (0, 0).\n";
         break;
-    case $y == 0:
+    case $y === 0.0:
         echo "Точка ($x, $y) лежит на оси абсцисс (оси X).\n";
         break;
-    case $x == 0:
+    case $x === 0.0:
         echo "Точка ($x, $y) лежит на оси ординат (оси Y).\n";
         break;
     case $x > 0 && $y > 0:
